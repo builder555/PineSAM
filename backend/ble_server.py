20d7b55406395876321328e9399fd8d640a157e1
builder555 <85308587+builder555@users.noreply.github.com>
2023-02-11 17:50:10 -0500

    compatible with v2.21
    - names are now mapped in the backend
    - detects version for backwards compatibility
    - cleaned up some code
    - exposed Reset To Factory Settings in ui

--- backend/ble_server.py
+++ backend/ble_server.py
@@ -4,8 +4,9 @@ import os
 import websockets
 import websockets.exceptions
 import json
+from ble import DeviceNotFoundException
 from pinecil_ble import Pinecil
-from pinecil_ble import DeviceNotFoundException
+from pinecil_ble import InvalidSettingException
 from pinecil_ble import ValueOutOfRangeException
 
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO').upper()
@@ -14,35 +15,38 @@ logging.basicConfig(level=LOG_LEVEL, format='%(asctime)s.%(msecs)03d::%(levelnam
 
 pinecil = Pinecil()
 
+async def process_command(command: str, payload: dict) -> dict:
+    if not pinecil.is_connected:
+        await pinecil.connect()
+    if command == 'GET_SETTINGS':
+        settings = await pinecil.get_all_settings()
+        return {'status': 'OK', 'payload': settings}
+    if command == 'UPDATE_SETTING':
+        logging.info("SETTING VALUE")
+        await pinecil.set_one_setting(payload['name'], payload['value'])
+        if payload.get('save'):
+            logging.info("SAVING TO FLASH")
+            await pinecil.save_to_flash()
+        return {'status': 'OK'}
+    return {'status': 'ERROR', 'message': 'Unknown command'}
+
 async def handle_message(websocket, data):
     logging.info(f'Got message: {data}') 
     json_data = json.loads(data)
-    command, payload = json_data['command'], json_data.get('payload', None)
+    command, payload = json_data['command'], json_data.get('payload', {})
     try:
-        if command == 'GET_SETTINGS':
-            settings = await pinecil.get_all_settings()
-            response = {'status': 'OK', 'payload': settings}
-        elif command == 'UPDATE_SETTING':
-            logging.info("SETTING VALUE")
-            await pinecil.set_one_setting(payload['uuid'], payload['value'])
-            if payload.get('save'):
-                logging.info("SAVING TO FLASH")
-                await pinecil.set_one_setting('00000000-0000-1000-8000-00805f9b34fb', 1)
-            response = {'status': 'OK'}
-        else:
-            response = {'status': 'ERROR', 'message': 'Unknown command'}
+        response = await process_command(command, payload)
         logging.info(f'Sending response')
-        await websocket.send(json.dumps({**response, 'command': command}))
-    except DeviceNotFoundException:
-        logging.warning('Device not found')
-        response = {'status': 'ERROR', 'message': 'Device not found'}
-        await websocket.send(json.dumps({**response, 'command': command}))
-    except ValueOutOfRangeException:
-        logging.warning('Value out of range')
-        response = {'status': 'ERROR', 'message': 'Value out of range'}
-        await websocket.send(json.dumps({**response, 'command': command}))
+    except (
+            InvalidSettingException,
+            DeviceNotFoundException,
+            ValueOutOfRangeException,
+            ) as e:
+        logging.warning(e.message)
+        response = {'status': 'ERROR', 'message': e.message}
+    await websocket.send(json.dumps({**response, 'command': command}))
 
-async def hello(websocket, path):
+async def ws_handler(websocket, path):
     try:
         while True:
             message = await websocket.recv()
@@ -50,7 +54,6 @@ async def hello(websocket, path):
     except websockets.exceptions.ConnectionClosed:
         logging.info('Connection closed')
 
-
-start_server = websockets.serve(hello, '0.0.0.0', 12999) #type: ignore
+start_server = websockets.serve(ws_handler, '0.0.0.0', 12999) #type: ignore
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
