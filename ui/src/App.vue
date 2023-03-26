873a543125ddb7d84ddbd639f8345382e2b2aeea
builder555 <85308587+builder555@users.noreply.github.com>
2023-03-26 10:54:28 -0400

    fix: display work hud inline with graph

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -32,16 +32,23 @@ onBeforeUnmount(() => {
       <div v-show="store.isBusy" class="spinner"><div></div></div>
       <div class="column is-half is-full-mobile has-text-right-tablet has-text-left"></div>
     </div>
-    <div class="is-flex">
-      <the-graph-view/>
+    <div class="columns is-multiline is-justify-content-center mb-5">
+      <div class="column is-hidden-portrait is-6 m-0 is-8-widescreen">
+        <the-graph-view/>
+      </div>
+      <div class="column is-flex is-6 m-0 is-4-widescreen is-justify-content-center">
+        <the-work-view />
+      </div>
     </div>
-    <div class="is-flex is-justify-content-center mb-6">
-      <the-work-view />
-    </div>
-    <the-settings />
+    <the-settings/>
   </div>
 </template>
 <style scoped>
+@media (orientation: portrait) {
+  .is-hidden-portrait {
+    display: none;
+  }
+}
 @keyframes spinner {
   to {
     transform: rotate(360deg);
