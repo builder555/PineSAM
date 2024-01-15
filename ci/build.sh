10af24ad21b5d7437f173686db01c3a4a648878c
Robert'); DROP TABLE students; -- <85308587+builder555@users.noreply.github.com>
2024-01-14 20:38:05 -0500

    fix: add dbus module

--- ci/build.sh
+++ ci/build.sh
@@ -4,5 +4,5 @@ pushd ui || exit 2
 npm run build
 popd || exit 2
 
-pyinstaller backend/main_server.py
+pyinstaller backend/main_server.py --collect-submodules dbus_fast
 pyinstaller --add-data ui/dist:ui ui/serve.py
