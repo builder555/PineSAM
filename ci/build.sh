56c37d10e21a6a9caf7c42195ae8d0395c4cf565
builder555 <85308587+builder555@users.noreply.github.com>
2024-01-24 12:26:19 -0500

    fix: update building process

--- ci/build.sh
+++ ci/build.sh
@@ -6,4 +6,4 @@ pushd ui || exit 2
 npm run build
 popd || exit 2
 
-pyinstaller --onefile --add-data "./version.txt:/" --add-data "./ui/dist:/gui" backend/main.py --collect-submodules dbus_fast 
+pyinstaller --onefile --add-data "./version.txt:./" --add-data "./ui/dist:./gui" backend/main.py --collect-submodules dbus_fast 
