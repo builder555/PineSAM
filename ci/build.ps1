278a758c0883d4e01de6b427f96aa622c7acac23
builder555 <85308587+builder555@users.noreply.github.com>
2024-01-23 21:46:13 -0500

    fix: update builder for windows

--- ci/build.ps1
+++ ci/build.ps1
@@ -3,7 +3,4 @@ Push-Location ui
 npm run build
 Pop-Location
 
-pyinstaller --onefile --add-data "./version.txt;/" --add-data "./ui/dist;/ui" backend/main.py
-
-# pyinstaller backend/main_server.py
-# pyinstaller --add-data "ui/dist;ui" ui/serve.py
+pyinstaller --onefile --add-data "./version.txt;/" --add-data "./ui/dist;/gui" backend/main.py
